e767ad8a5e2753c12c8f404f90c7879b7a4154db
Naominm <naomimbugua536@gmail.com>
2025-06-24 10:23:52 +0300

    Build the image section

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,27 @@
 import { Box } from "@mui/material";
+import LoginImage from "../assets/2.jpg"
 
 function LoginPage() {
-  return <Box component="div">Hello this is thee login page</Box>;
+  return(
+   <Box component="div" sx={{display:"flex",alignItems:"center", height:"100vh"}}>
+    <ImageSection/>
+    <FormSection/>
+   </Box> 
+  )
+}
+
+function ImageSection(){
+    return(
+        <Box component="div" sx={{height:"auto", width:"50%", overflow:"hidden", mt:2}}>
+            <Box component="img" src={LoginImage} sx={{width:"100%", height:"100%", objectFit:"cover"}}/>
+        </Box>
+    )
+}
+
+function FormSection(){
+    return(
+        <Box component="div"></Box>
+    )
 }
 
 export default LoginPage;
