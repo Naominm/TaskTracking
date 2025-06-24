fbb19d4b1494bb665805a1a9c0b76685da69841f
Naominm <naomimbugua536@gmail.com>
2025-06-24 12:01:23 +0300

    Build the Text section

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,27 +1,88 @@
-import { Box } from "@mui/material";
-import LoginImage from "../assets/2.jpg"
+import { Box, Typography, Paper, TextField, Button } from "@mui/material";
+import LoginImage from "../assets/6378759.jpg";
 
 function LoginPage() {
-  return(
-   <Box component="div" sx={{display:"flex",alignItems:"center", height:"100vh"}}>
-    <ImageSection/>
-    <FormSection/>
-   </Box> 
-  )
+  return (
+    <Box
+      component="div"
+      sx={{
+        display: "flex",
+        gap: "1rem",
+        alignItems: "center",
+        height: "100vh",
+        mt: 2,
+        mx: 4,
+      }}
+    >
+      <ImageSection />
+      <FormSection />
+    </Box>
+  );
 }
 
-function ImageSection(){
-    return(
-        <Box component="div" sx={{height:"auto", width:"50%", overflow:"hidden", mt:2}}>
-            <Box component="img" src={LoginImage} sx={{width:"100%", height:"100%", objectFit:"cover"}}/>
-        </Box>
-    )
+function ImageSection() {
+  return (
+    <Box component="div" sx={{ height: "29rem ", width: "50%", mx: 2 }}>
+      <Box
+        component="img"
+        src={LoginImage}
+        sx={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          objectPosition: "top",
+          borderRadius: "1rem",
+        }}
+      />
+    </Box>
+  );
 }
 
-function FormSection(){
-    return(
-        <Box component="div"></Box>
-    )
+function FormSection() {
+  return (
+    <Paper
+      component="div"
+      sx={{ width: "50%", height: "29rem", padding: "2rem" }}
+    >
+      <Typography
+        variant="h5"
+        sx={{
+          mt: 6,
+          color: "var(--secondary-color)",
+          fontFamily: "var(--primary-font)",
+          fontWeight: 700,
+        }}
+      >
+        {" "}
+        Welcome to Task Manager
+      </Typography>
+      <Typography variant="body2" sx={{ color: "var(--text-color)" }}>
+        stay organized
+      </Typography>
+      <Box
+        component="form"
+        sx={{ mt: 5, display: "flex", flexDirection: "column", gap: "2rem" }}
+      >
+        <TextField
+          required
+          variant="outlined"
+          label="Email Address"
+          size="small"
+        >
+          Email Address
+        </TextField>
+        <TextField required variant="outlined" label="password" size="small">
+          Password
+        </TextField>
+        <Button
+          variant="contained"
+          sx={{ backgroundColor: "var(--secondary-color)" }}
+        >
+          submit
+        </Button>
+      </Box>
+    </Paper>
+  );
 }
 
 export default LoginPage;
